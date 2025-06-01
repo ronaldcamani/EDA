6b2d3819e07e11186f515e1900ae30706151262d
ronalcamani <rcamani@unsa.edu.pe>
2025-05-31 21:58:12 -0500

    Ejercicio Propuesto 2 y 4 Corregidos

--- Lab02/EjerciciosPropuestos/Ejercicio2.java
+++ Lab02/EjerciciosPropuestos/Ejercicio2.java
@@ -8,27 +8,27 @@ public class Ejercicio2 {
         }
         return Ainvertido;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el tamaño del arreglo: ");
         int n = scanner.nextInt();
-        
+
         int[] A = new int[n];
         System.out.println("Ingrese los elementos del arreglo:");
         for (int i = 0; i < n; i++) {
             A[i] = scanner.nextInt();
         }
-        
+
         Ejercicio2 ejercicio = new Ejercicio2();
         int[] resultado = ejercicio.rotarIzquierdaArray(A);
-        
+
         System.out.print("Arreglo rotado: ");
         for (int num : resultado) {
             System.out.print(num + " ");
         }
         System.out.println();
-        
+
         scanner.close();
     }
-} 
+}
