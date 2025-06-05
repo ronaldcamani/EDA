015ed769a43a3f7604260b4c1b063094db55bd6f
ronalcamani <rcamani@unsa.edu.pe>
2025-06-05 09:22:36 -0500

    MAin.java y StackList.java creados y testeados

---
+++ Lab05-PilasColas/EjerciciosResueltos/StackList.java
@@ -0,0 +1,51 @@
+public class StackList<T> {
+    private Node<T> top;
+    private int size;
+
+    public StackList() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(T element) {
+        Node<T> newNode = new Node<>(element);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T element = top.data;
+        top = top.next;
+        size--;
+        return element;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void display() {
+        Node<T> current = top;
+        System.out.print("Stack: ");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+} 
