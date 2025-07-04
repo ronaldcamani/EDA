1da43977ada78d4e721d7f83517eabc20e5875cb
ronalcamani <rcamani@unsa.edu.pe>
2025-07-03 22:29:52 -0500

    correecion; eliminando librerias insnecesarioas

--- Lab08-B_B+/BTree.java
+++ Lab08-B_B+/BTree.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
-
 public class BTree<E extends Comparable<E>> {
-  private BNode<E> root;
-  private int orden;
-  private boolean up;
-  private BNode<E> nDes;
+    private BNode<E> root;
+    private int orden;
+    private boolean up;
+    private BNode<E> nDes;
   public BTree(int orden) {
   this.orden = orden;
   this.root = null;
