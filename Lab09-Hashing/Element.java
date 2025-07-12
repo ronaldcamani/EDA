a4ee9702e46ea1a31c785680756755325a5b7910
ronalcamani <rcamani@unsa.edu.pe>
2025-07-11 23:42:41 -0500

    correcion de identacion y testeado

--- Lab09-Hashing/Element.java
+++ Lab09-Hashing/Element.java
@@ -1,50 +1,50 @@
 public class Element<T> {
-    private Register<T> data;
-    private boolean deleted;
+  private Register<T> data;
+  private boolean deleted;
 
-    public Element() {
-        this.data = null;
-        this.deleted = false;
-    }
+  public Element() {
+    this.data = null;
+    this.deleted = false;
+  }
 
-    public Element(Register<T> data) {
-        this.data = data;
-        this.deleted = false;
-    }
+  public Element(Register<T> data) {
+    this.data = data;
+    this.deleted = false;
+  }
 
-    public Register<T> getData() {
-        return data;
-    }
+  public Register<T> getData() {
+    return data;
+  }
 
-    public void setData(Register<T> data) {
-        this.data = data;
-        this.deleted = false;
-    }
+  public void setData(Register<T> data) {
+    this.data = data;
+    this.deleted = false;
+  }
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+  public boolean isDeleted() {
+    return deleted;
+  }
 
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
+  public void setDeleted(boolean deleted) {
+    this.deleted = deleted;
+  }
 
-    public boolean isEmpty() {
-        return data == null;
-    }
+  public boolean isEmpty() {
+    return data == null;
+  }
 
-    public boolean isAvailable() {
-        return isEmpty() || deleted;
-    }
+  public boolean isAvailable() {
+    return isEmpty() || deleted;
+  }
 
-    @Override
-    public String toString() {
-        if (isEmpty()) {
-            return "[ EMPTY ]";
-        } else if (deleted) {
-            return "[ DELETED ]";
-        } else {
-            return "[ " + data.toString() + " ]";
-        }
+  @Override
+  public String toString() {
+    if (isEmpty()) {
+      return "[ EMPTY ]";
+    } else if (deleted) {
+      return "[ DELETED ]";
+    } else {
+      return "[ " + data.toString() + " ]";
     }
+  }
 }
